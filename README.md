028b8acc2396ab92856ab28447122da3191ab5e5
Alan Dust <adust1@gmail.com>
2018-12-11 14:25:09 -0500

    metronome is fully functional now

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 Metronome
 
-This is my first attempt at a basic metronome.  
-
+This is a basic metronome for musicians to keep a steady beat.
 
 Technologies utilized:
 Ruby 2.4.5
@@ -9,6 +8,6 @@ Rails 5.2.1
 Javascript
 React
 Make_It_So gem
-rspec
-jasmine
-postgresql
+AWS
+
+I made this application utilizing Rails because I  might want to add on features in the future such as storing practice times, tempos, etc.  For now, it is a functional basic metronome application.
--- app/javascript/react/containers/MetronomeContainer.js
+++ app/javascript/react/containers/MetronomeContainer.js
@@ -6,11 +6,52 @@ class MetronomeContainer extends Component {
 
     this.state = {
       playing: false,
-      count: 0,
-      bpm: 100,
-      beatsPerMeasure: 4
+      bpm: 120,
     };
+    this.click = new Audio("https://s3.amazonaws.com/metronome-production/metronome+sounds/clickDownbeat.wav");
+    this.clickDownbeat = new Audio("https://s3.amazonaws.com/metronome-production/metronome+sounds/metronome_click.wav");
   }
+
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+
+    if (this.state.playing) {
+      clearInterval(this.timer);
+      this.timer = setInterval(this.playClick, (60 / bpm) * 1000);
+
+      this.setState({
+        count: 0,
+        bpm
+      });
+    } else {
+      this.setState({ bpm });
+    }
+  };
+
+  startStop = () => {
+    if (this.state.playing) {
+      clearInterval(this.timer);
+      this.setState({
+        playing: false
+      });
+    } else {
+      this.timer = setInterval(
+        this.playClick,
+        (60 / this.state.bpm) * 1000
+      );
+      this.setState(
+        {
+          count: 0,
+          playing: true
+        },
+        this.playClick
+      );
+    }
+  };
+
+  playClick = () => {
+    this.click.play();
+  };
 
   render() {
     const { playing, bpm } = this.state;
@@ -19,9 +60,11 @@ class MetronomeContainer extends Component {
       <div className="metronome">
         <div className="beats-per-minute">
           <div>{bpm} BPM</div>
-          <input type="range" min="40" max="260" value={bpm} />
+          <input type="range" min="40" max="260" value={bpm} onChange={this.handleBpmChange}/>
         </div>
-        <button>{playing ? 'Stop' : 'Start'}</button>
+        <button className="start-stop" onClick={this.startStop}>
+          {playing ? 'Stop' : 'Start'}
+        </button>
       </div>
     );
   }
